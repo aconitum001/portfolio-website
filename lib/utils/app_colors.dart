6c9f7064e5224448e83c8ac45da0c3a25925749b
aconitum001 <skandern0000@gmail.com>
2025-01-15 11:05:19 +0100

    created app styles & app themes

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const PRIMARY_COLOR = Colors.black;
+  static const SECOUNDARY_COLOR = Colors.white;
+  static const MAIN_GRAY_COLOR = Color(0xff71717A);
+  static const SECOUNDARY_GRAY_COLOR = Color(0xffD4D4D8);
+}
--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/utils/app_colors.dart';
 
 abstract class AppTheme {
   static ThemeData lightTheme = ThemeData(
     fontFamily: "",
     scaffoldBackgroundColor: Colors.white,
-    colorScheme: const ColorScheme.light(),
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.PRIMARY_COLOR,
+      secondary: AppColors.SECOUNDARY_COLOR,
+      onPrimary: AppColors.SECOUNDARY_GRAY_COLOR,
+      onSecondary: AppColors.MAIN_GRAY_COLOR,
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
