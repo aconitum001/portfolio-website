f44185308cb4b7ad8c489da094e48685ad55e768
aconitum001 <skandern0000@gmail.com>
2025-01-15 11:26:45 +0100

    created assets folder & added images

---
+++ lib/utils/assets.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imageIconsDiscord
+  /// assets/icons/discord.svg
+  static const String imageIconsDiscord = "assets/icons/discord.svg";
+
+  /// Assets for imageIconsDownload
+  /// assets/icons/download.svg
+  static const String imageIconsDownload = "assets/icons/download.svg";
+
+  /// Assets for imageIconsFb
+  /// assets/icons/fb.svg
+  static const String imageIconsFb = "assets/icons/fb.svg";
+
+  /// Assets for imageIconsRedit
+  /// assets/icons/redit.svg
+  static const String imageIconsRedit = "assets/icons/redit.svg";
+
+  /// Assets for imageIconsTwitter
+  /// assets/icons/twitter.svg
+  static const String imageIconsTwitter = "assets/icons/twitter.svg";
+
+  /// Assets for imageImagesAboutMe
+  /// assets/images/about_me.svg
+  static const String imageImagesAboutMe = "assets/images/about_me.svg";
+
+  /// Assets for imageImagesBanner
+  /// assets/images/Banner.svg
+  static const String imageImagesBanner = "assets/images/Banner.svg";
+
+  /// Assets for imageImagesGit
+  /// assets/images/git.svg
+  static const String imageImagesGit = "assets/images/git.svg";
+
+  /// Assets for imageImagesJavscript
+  /// assets/images/javscript.svg
+  static const String imageImagesJavscript = "assets/images/javscript.svg";
+
+  /// Assets for imageImagesLogo
+  /// assets/images/logo.svg
+  static const String imageImagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imageImagesNest
+  /// assets/images/nest.svg
+  static const String imageImagesNest = "assets/images/nest.svg";
+
+  /// Assets for imageImagesSass
+  /// assets/images/sass.svg
+  static const String imageImagesSass = "assets/images/sass.svg";
+
+  /// Assets for imageImagesStorybook
+  /// assets/images/storybook.svg
+  static const String imageImagesStorybook = "assets/images/storybook.svg";
+}
+
